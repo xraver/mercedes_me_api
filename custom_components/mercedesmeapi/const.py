c5f02fafa6f3304d252cfde53c4555f219824591
Giorgio Ravera <giorgio.ravera@gmail.com>
2020-12-17 09:48:05 +0100

    Updated to version 0.7

--- custom_components/mercedesmeapi/const.py
+++ custom_components/mercedesmeapi/const.py
@@ -9,7 +9,7 @@ https://github.com/xraver/mercedes_me_api/
 # Software Parameters
 NAME = "Mercedes Me API"
 DOMAIN = "mercedesmeapi"
-VERSION = "0.6"
+VERSION = "0.7"
 TOKEN_FILE = ".mercedesme_token"
 CREDENTIAL_FILE = ".mercedesme_credentials"
 RESOURCES_FILE = ".mercedesme_resources"
